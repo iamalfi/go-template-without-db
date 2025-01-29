b0cf1099756bd3388623fa1a60bf6d4881fd6819
Alfiya <alfiya96960@gmail.com>
2025-01-29 18:39:42 +0530

    review code

--- model/aadhaar_data.go
+++ model/aadhaar_data.go
@@ -16,18 +16,17 @@ type Address struct {
 	Vtc         string `bson:"vtc,omitempty" json:"vtc,omitempty"`
 }
 type Aadhaar_Data struct {
-	ID          string   `bson:"_id,omitempty" json:"id,omitempty"`
-	AadhaarNo   string   `bson:"aadhaar_no,unique" json:"aadhaar_number,omitempty"`
-	Name        *string  `bson:"name" json:"name,omitempty"`
-	Gender      *string  `bson:"gender" json:"gender,omitempty"`
-	Address     *Address `bson:"address" json:"address,omitempty"`
-	FullAddress *string  `bson:"full_address" json:"full_address,omitempty"`
-	CareOf      *string  `bson:"care_of" json:"care_of,omitempty"`
-	DOB         *string  `bson:"date_of_birth" json:"date_of_birth,omitempty"`
-	ShareCode   *string  `bson:"share_code" json:"share_code,omitempty"`
-	Status      *string  `bson:"status" json:"status,omitempty"`
-	YOB         *int     `bson:"year_of_birth" json:"year_of_birth,omitempty"`
-	// Data      interface{} `bson:"data" json:"data,omitempty"`
-	CreatedAt time.Time `bson:"created_at" json:"created_at,omitempty"`
-	UpdatedAt time.Time `bson:"updated_at" json:"updated_at,omitempty"`
+	ID          string    `bson:"_id,omitempty" json:"id,omitempty"`
+	AadhaarNo   string    `bson:"aadhaar_no,unique" json:"aadhaar_number,omitempty"`
+	Name        *string   `bson:"name" json:"name,omitempty"`
+	Gender      *string   `bson:"gender" json:"gender,omitempty"`
+	Address     *Address  `bson:"address" json:"address,omitempty"`
+	FullAddress *string   `bson:"full_address" json:"full_address,omitempty"`
+	CareOf      *string   `bson:"care_of" json:"care_of,omitempty"`
+	DOB         *string   `bson:"date_of_birth" json:"date_of_birth,omitempty"`
+	ShareCode   *string   `bson:"share_code" json:"share_code,omitempty"`
+	Status      *string   `bson:"status" json:"status,omitempty"`
+	YOB         *int      `bson:"year_of_birth" json:"year_of_birth,omitempty"`
+	CreatedAt   time.Time `bson:"created_at" json:"created_at,omitempty"`
+	UpdatedAt   time.Time `bson:"updated_at" json:"updated_at,omitempty"`
 }
